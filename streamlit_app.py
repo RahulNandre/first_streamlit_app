3c3a615bd5b4ee3d77c5718a7746a7d7413c5ca0
Rahul Nandre <45839524+RahulNandre@users.noreply.github.com>
2022-10-04 14:54:30 +0530

    Update streamlit_app.py
    pandas csv file reading function added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('My parents healthy diner')
 streamlit.header('Breakfasr Menu')
@@ -6,3 +7,6 @@ streamlit.text('🥣 Idli')
 streamlit.text('🥗 Dosa')
 streamlit.text('🍞 puri')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
