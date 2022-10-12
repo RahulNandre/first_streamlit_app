cad289657e79902190fe434a8d7ca544979ae00f
Rahul Nandre <45839524+RahulNandre@users.noreply.github.com>
2022-10-12 11:14:29 +0530

    Update streamlit_app.py
    function created to get response

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,15 +17,18 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
+def getfruitvicedata(fruitname):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+str(fruitname))
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+
 streamlit.header('Fruityvice Fruit Advice')
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error("Please select fruit to get information")
   else:
-#     streamlit.write('The user entered ', fruit_choice)
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+str(fruit_choice))
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    back_from_function = getfruitvicedata(fruit_choice)
     streamlit.dataframe(fruityvice_normalized)
 
 except Exception as e:
